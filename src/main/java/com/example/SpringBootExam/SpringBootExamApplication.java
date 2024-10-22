41969ac2821b3bc7a40b1b5fa460c4c256378a9a
tefa0408 <estefanyalfaro2230@gmail.com>
2024-10-22 00:08:29 -0500

    persona y direccion entities

---
+++ src/main/java/com/example/SpringBootExam/SpringBootExamApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootExam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootExamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringBootExam/SpringBootExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootExam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
